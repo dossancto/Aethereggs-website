07080db7c7175370fe61ee61f0ad0d33bd9f7522
lu-css <dossantossilvalucas963@gmail.com>
2022-12-15 12:59:53 -0300

    prepare to try host

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
 
   # Aether Eggs Pages
   scope 'eggs' do
+    # TUTORIAL PAGES
     get 'tutorial', to: 'tutorial_maps#index', as: 'tutorial'
     get 'tutorial/:mapname', to: 'tutorial_maps#map', as: 'tutorial_map'
     get 'tutorial/:mapname/:type/:id', to: 'tutorial_maps#steps', as: 'tutorial_steps'
     get 'tutorial/:mapname/:type/:id/edit', to: 'tutorial_maps#edit', as: 'tutorial_edit'
+
+    # Bot Pages
+    #
+    get 'bot', to: 'bot#index', as: 'bot'
   end
 
   scope 'cup' do
