ebe524ed2e4990b9616abb41409430b87432326f
lu-css <dossantossilvalucas963@gmail.com>
2022-12-15 11:02:17 -0300

    add a scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'tutorial_maps#index'
 
-  # Tutorial Pages
-  get 'tutorial', to: 'tutorial_maps#index'
-  get 'tutorial/:mapname', to: 'tutorial_maps#map', as: 'tutorial_map'
-  get 'tutorial/:mapname/:type/:id', to: 'tutorial_maps#steps', as: 'tutorial_steps'
+  # Aether Eggs Pages
+  scope 'eggs' do
+    get 'tutorial', to: 'tutorial_maps#index', as: 'tutorial'
+    get 'tutorial/:mapname', to: 'tutorial_maps#map', as: 'tutorial_map'
+    get 'tutorial/:mapname/:type/:id', to: 'tutorial_maps#steps', as: 'tutorial_steps'
+    get 'tutorial/:mapname/:type/:id/edit', to: 'tutorial_maps#edit', as: 'tutorial_edit'
+  end
 
-  # Edit Tutorial Pages
-  get 'tutorial/:mapname/:type/:id/edit', to: 'tutorial_maps#edit', as: 'tutorial_edit'
+  scope 'cup' do
+  end
 end
